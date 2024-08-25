54284169b21a85380c277b7ceba96dc7b812263b
Imam Usmani <imam@e.email>
2024-08-25 20:37:40 +0200

    load comic and resize

--- src/epaper_xkcd.py
+++ src/epaper_xkcd.py
@@ -1,4 +1,10 @@
+import tempfile
+import os
+
+
 import epaper
+import xkcd
+from PIL import Image
 
 
 def display_last_xkcd():
@@ -6,9 +12,25 @@ def display_last_xkcd():
 
     """
     epd = epaper.epaper('epd2in13_V4').EPD()
-
     epd.init()
     epd.Clear()
+    epd_ratio = epd.width / epd.height
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        comic = xkcd.getLatestComic()
+        comic.download(tmpdirname, 'image')
+        fp = os.path.join(tmpdirname, 'image')
+        img = Image.open(fp)
+        print(img.size)
+        img_ratio = img.width / img.height
+        if (epd_ratio)*(img_ratio-1)<0:
+            img = img.rotate(90, expand=True)
+        maxsize = epd.width, epd.height
+        img.thumbnail(maxsize)
+        print(img.size)
+
+
+
 
     epd.sleep()
 
